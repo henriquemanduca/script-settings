c4ea4bf49afcccf54f530f4957faa3ae22537e85
Henrique <henrique@pop-os.localdomain>
2020-09-25 16:44:04 -0300

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# script-settins
-A Ubuntu based script for default settins
+# script-settings
+A Ubuntu based script for default settings.
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+After a fresh linux installation, we need to make the same predictable settings, this script aims to save us some time.
+
+
+
+### ⚠️ Tested only with Ubuntu and Pop OS.
+
+Other distributions might need other commands.
+
+
+
+## What does it do
+
+### Remove
+
+* LibreOffice
+
+### Install
+
+* fonts-firacode
+* typora
+* simple note
+* google chrome (stable current)
+* docker
+* gnome-boxes
+* brave-browser
+* font-manager
+* zsh
+* ca-certificates
+* docker-ce
+* Vs Code (1.49)
+* DBeaver (7.2.1)
+
+### Settings
+
+* Upgrades
+* Adds user to the Docker group (Docker commands without ***sudo***)
+* Creats a deffault ssh key
+
+
+
+## Using
+
+1. Open the file properties from `script-settings.sh`.
+
+2. Set the checkbox that allow execute the file as a programm.
+
+3. On terminal type:
+
+   ```
+   $ sudo ./script-settings.sh
+   ```
+
+4. Go grab some coffee...
+
+
+
+
+
---
+++ script-settings.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+# ----------------------------- VARIABLES ----------------------------- #
+URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+URL_SIMPLE_NOTE="https://github.com/Automattic/simplenote-electron/releases/download/v1.8.0/Simplenote-linux-1.8.0-amd64.deb"
+URL_DBEAVER="https://dbeaver.io/files/7.2.1/dbeaver-ce_7.2.1_amd64.deb"
+VS_CODE="https://az764295.vo.msecnd.net/stable/e5e9e69aed6e1984f7499b7af85b3d05f9a6883a/code_1.49.2-1600965325_amd64.deb"
+DOWNLOADS="$HOME/Downloads/programs"
+
+PROGRAMS=(
+  gnome-boxes
+  brave-browser
+  font-manager
+  zsh
+  typora
+  ca-certificates
+  docker-ce
+)
+
+# ----------------------------- REQUIREMENTS ----------------------------- #
+
+## Removing any locks on the apt ##
+sudo rm /var/lib/dpkg/lock-frontend
+sudo rm /var/cache/apt/archives/lock
+
+timedatectl set-local-rtc 1 --adjust-system-clock
+
+# ----------------------------- REMOVE ----------------------------- #
+echo ""
+echo ""
+echo "Remove libreoffice"
+sudo apt-get remove --purge libreoffice* -y
+
+sudo apt clean
+sudo apt-get autoremove
+
+echo "Install Fira Code"
+sudo apt install fonts-firacode
+
+echo ""
+echo ""
+## Adding third-party repositories##
+sudo apt install apt-transport-https curl wget
+
+# add Typora's repository
+wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+sudo add-apt-repository 'deb https://typora.io/linux ./'
+
+# add Doker's repository
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+# ----------------------------- EXECUTION ----------------------------- #
+## Updating the repository after adding new repositories ##
+echo ""
+echo ""
+sudo apt update -y
+sudo apt upgrade -y
+
+echo ""
+echo ""
+## Download and installation ##
+mkdir "$DOWNLOADS"
+wget -c "$URL_GOOGLE_CHROME"  -P "$DOWNLOADS"
+wget -c "$URL_SIMPLE_NOTE"    -P "$DOWNLOADS"
+wget -c "$URL_DBEAVER"        -P "$DOWNLOADS"
+
+echo ""
+echo ""
+## Installing .deb packages downloaded in the previous session ##
+sudo dpkg -i $DOWNLOADS/*.deb
+sudo apt-get -f install -y
+
+curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+# Install programs in apt
+for program_name in ${PROGRAMS[@]}; do
+  if ! dpkg -l | grep -q $program_name; then # Only install if not already installed
+    apt install "$program_name" -y
+  else
+    echo "[INSTALADO] - $program_name"
+  fi
+done
+
+echo ""
+echo ""
+
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+
+
+# ----------------------------- AFTER-INSTALLATION ----------------------------- #
+## Finalization, update and cleaning ##
+sudo apt update && sudo apt dist-upgrade -y
+
+sudo apt autoclean
+sudo apt autoremove -y
+
+# ----------------------------- OTHERS SETTINGS ----------------------- #
+## Docker without sudo
+sudo usermod -aG docker ${USER} && su - ${USER}
+
+## SSH
+echo ""
+echo "Generate key ssh"
+ssh-keygen -t rsa -b 4096 -C "henriquemanduca@live.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+
+
+
